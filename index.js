28fcc5e1e6de3cbac24ea022d06d61287e39f1ea
abdulbasid <abdulbasidhussain9@gmail.com>
2025-07-26 16:07:24 -0700

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'https://bootcamp-pearl-phi.vercel.app/'],
   credentials: true
 }));
 
